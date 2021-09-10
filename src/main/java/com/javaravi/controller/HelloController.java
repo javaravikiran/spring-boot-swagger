f3c1e26f76a08f039b2eb5f560a572b593e223ef
javaravikiran <ravikiran.namana@gmail.com>
2021-09-10 18:32:23 +0530

    initial commit

---
+++ src/main/java/com/javaravi/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.javaravi.controller;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/api/javaravi")
+public class HelloController {
+	
+	@GetMapping(value = "/welcome")
+	public String sayHello() {
+		return "Swagger Hello World";
+	}
+	
+	@GetMapping("/getDetails")
+	public List<String> getDetails() {
+		List<String> li=new ArrayList<>();
+		li.add("Ravi");
+		li.add("Priya");
+		li.add("Hasvika");
+		li.add("Lakshmi");
+		return li;
+	}
+}

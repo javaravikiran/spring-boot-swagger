cb0d80d71ff167fb49999eb696d248664df9b6a2
javaravikiran <ravikiran.namana@gmail.com>
2021-09-10 18:48:52 +0530

    code changes added

--- src/main/java/com/javaravi/demo/SpringBootSwaggerDemoApplication.java
+++ src/main/java/com/javaravi/demo/SpringBootSwaggerDemoApplication.java
@@ -2,11 +2,13 @@ package com.javaravi.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@ComponentScan(basePackages = "com.javaravi.config,com.javaravi.controller")
 public class SpringBootSwaggerDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSwaggerDemoApplication.class, args);
